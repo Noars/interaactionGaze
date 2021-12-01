202d881cf7cbd7a85348f96ec60c989b6a5cf810
kuhlkrein <sebastien.riou@univ-grenoble-alpes.fr>
2021-12-01 16:35:39 +0100

    auto reformat code

--- untitled/src/main/java/gaze/devicemanager/TobiiGazeDeviceManager.java
+++ untitled/src/main/java/gaze/devicemanager/TobiiGazeDeviceManager.java
@@ -50,7 +50,6 @@ public class TobiiGazeDeviceManager extends AbstractGazeDeviceManager {
                             try {
                                 Thread.sleep(10);
                             } catch (InterruptedException e) {
-                                // TODO Auto-generated catch block
                                 e.printStackTrace();
                             }
 
