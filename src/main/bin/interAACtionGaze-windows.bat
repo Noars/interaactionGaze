7567fdcdbd5873f1d41459abaf245ec3d24674d6
Noars <55838787+Noars@users.noreply.github.com>
2022-08-12 15:31:05 +0200

    Fix sccript windows

--- src/main/bin/interAACtionGaze-windows.bat
+++ src/main/bin/interAACtionGaze-windows.bat
@@ -1,8 +1,8 @@
 @echo on
 
-SET JAVA_HOME=..\lib\jre
+SET SCRIPT_PATH=%~dp0
+SET JAR_PATH="%SCRIPT_PATH%..\lib\interAACtionGaze.jar"
+SET JAVA_HOME=%SCRIPT_PATH%..\lib\jre
 SET PATH=%JAVA_HOME%\bin;%PATH%;%LocalAppData%\TobiiStreamEngineForJava\lib\tobii\x64
 
-java -cp "..\lib\*" -Xms256m -Xmx1g -Dlogging.appender.console.level=OFF -jar ../lib/interAACtionGaze.jar
-
-pause
+java -cp "..\lib\*" -Xms256m -Xmx1g -Dlogging.appender.console.level=OFF -jar %JAR_PATH%
