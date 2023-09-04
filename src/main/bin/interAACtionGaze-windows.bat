523a478e471946b70d4830b6c91251ed84a94b61
Noars <55838787+Noars@users.noreply.github.com>
2023-09-04 10:19:19 +0200

    minimize the terminal

--- src/main/bin/interAACtionGaze-windows.bat
+++ src/main/bin/interAACtionGaze-windows.bat
@@ -5,4 +5,4 @@ SET JAR_PATH="%SCRIPT_PATH%..\lib\interAACtionGaze.jar"
 SET JAVA_HOME=%SCRIPT_PATH%..\lib\jre
 SET PATH=%JAVA_HOME%\bin;%PATH%;%LocalAppData%\TobiiStreamEngineForJava\lib\tobii\x64
 
-java -cp "..\lib\*" -Xms256m -Xmx1g -Dlogging.appender.console.level=OFF -jar %JAR_PATH%
+start /min java -cp "..\lib\*" -Xms256m -Xmx1g -Dlogging.appender.console.level=OFF -jar %JAR_PATH%
